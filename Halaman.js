08989fdad507bf827a479b8e0aed68ced6b880c8
ranto69 <publisherblogger69@gmail.com>
2018-03-28 11:30:52 +0700

    Add files via upload

---
+++ Halaman.js
@@ -0,0 +1,98 @@
+<div class="text-center margin-bottom-20">
+<h3>Advertise</h3>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Ads_PostStmBois -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3726891448895332"
+     data-ad-slot="8046895421"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+  </div>
+
+<div class="row margin-bottom-20">
+  <div class="col-md-6">
+<div class="panel panel-primary">
+    <div class="panel-heading text-center"><h2><i class="fa fa-shield"></i> Safe Link Converter <i class="fa fa-shield"></i></h2></div>
+        <div class="panel-body text-center">
+          <p class="text-primary margin-bottom-20">Encrypting your link and protect the link from viruses, malware, thief, etc!<br/>Made your link safe to visit.</p>
+
+<div class="progress" id="daplong">
+    <div class="progress-bar progress-bar-striped active six-sec-ease-in-out" role="progressbar" data-transitiongoal="100"></div>
+</div>
+<button id="download2" class="alert alert-dismissible alert-success btn-lg" style='display:none'><i class="fa fa-thumbs-o-up" aria-hidden="true"></i> <strong>Well done!</strong> you have successfully gained access to Decrypted Link. <i class="fa fa-hand-o-down slideInDownThumb" aria-hidden="true"></i></button>
+
+        </div>
+      </div>
+</div>
+  <div class="col-md-6 text-left">
+<div class="panel panel-info">
+  <!-- Default panel contents -->
+  <div class="panel-heading"><h2><i class="fa fa-question-circle-o" aria-hidden="true"></i> How to use our tool:</h2></div>
+  <div class="panel-body">
+<ol>
+<li>Click on <strong>How To Use</strong> menu above.</li>
+<li>Click on the code and <kbd><kbd>CTRL</kbd> + <kbd>C</kbd></kbd> on your keyboard.</li>
+<li>Paste the code in your HTML blog theme before the <mark>&lt;/body&gt;</mark>.</li>
+<li>Save your HTML blog theme. you are done!</li>
+<li>Now, your blog's outbound links was encrypted!</li>
+</ol>
+  </div>
+</div>
+</div><div class="clear"></div>
+</div>
+
+<div class="row margin-bottom-20">
+  <div class="col-md-4 text-center">
+<h3>Advertise</h3>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Ads_PostStmBois -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3726891448895332"
+     data-ad-slot="8046895421"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
+  <div class="col-md-4 margin-top-25">
+<div class="panel panel-success text-center">
+<div class="panel-heading">
+<h3 class="panel-title">
+<b>Your link show here</b> <i aria-hidden="true" class="fa fa-hand-o-down"></i></h3>
+</div>
+<div class="panel-body">
+<script src="https://cdn.rawgit.com/ranto69/Safelinku/db129d63/Post2.js" type="text/javascript"></script>
+<script type="text/javascript">
+var currentURL=location.href;
+var str = currentURL;
+var res = str.replace("https://blogranweb.blogspot.com/p/generate.html?url=", "");
+document.write('<button type="button" id="download" class="Visit_Link btn btn-success" onclick="changeLink();" style="display: none;"><strong>Visit Link</strong> <i class="fa fa-external-link" aria-hidden="true"></i></button>')
+</script>
+  </div>
+</div>
+</div>
+ <div class="col-md-4 text-center">
+<h3>Advertise</h3>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Ads_PostStmBois -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3726891448895332"
+     data-ad-slot="8046895421"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
+<div class="clear"></div>
+</div>
+
+<div class="container">
+  <div class="text-left">
+
+  </div>
+</div>
